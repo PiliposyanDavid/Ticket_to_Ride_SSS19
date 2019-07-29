0344279c02d78aca6f61b1dc0b10750972c3613c
PiliposyanDavid <Piliposyan.David99@gmail.com>
2019-07-29 12:54:27 +0400

    add roms

--- server/components/players/Player.js
+++ server/components/players/Player.js
@@ -1,33 +1,27 @@
-'use strict';
-
-
 class Player {
 
+    constructor(player, client) {
+        this.client = client;
+        this.id = player.id;
+        this.name = player.name;
+        this.color = '';
+        this.cards = 0;
+        this.pieces = 0;
+        this.points = 0;
+        this.destinations = [];
+    }
 
-  constructor(player, client) {
-    this.client = client;
-    this.id = player.id;
-    this.name = player.name;
-    this.color = '';
-    this.cards = 0;
-    this.pieces = 0;
-    this.points = 0;
-    this.destinations = [];
-  }
-
-
-  simplify() {
-    return {
-      id: this.id,
-      name: this.name,
-      color: this.color,
-      cards: this.cards,
-      pieces: this.pieces,
-      points: this.points,
-      destinations: this.destinations,
-    };
-  }
-
+    simplify() {
+        return {
+            id: this.id,
+            name: this.name,
+            color: this.color,
+            cards: this.cards,
+            pieces: this.pieces,
+            points: this.points,
+            destinations: this.destinations,
+        };
+    }
 }
 
 
